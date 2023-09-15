6da3cd24c8804dd8f04cabd9cee26bd55ed369f5
unknown <briaglp2@gmail.com>
2023-09-15 15:13:44 +0200

    newsave

--- autogithub.py
+++ autogithub.py
@@ -2,6 +2,5 @@ import os
 
 
 os.system("git add .")
-info = input("Entrer les informations qui ont été modifier: ")
 os.system("git commit -m newsave ")
 os.system("git push -u PythonProject master")
