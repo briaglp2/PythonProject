7f2f41266c2e0ac293d207ceeb1851616df449b6
unknown <briaglp2@gmail.com>
2023-09-15 14:36:15 +0200

    Ajoutrécent

--- autogithub.py
+++ autogithub.py
@@ -3,5 +3,5 @@ import os
 
 os.system("git add .")
 info = input("Entrer les informations qui ont été modifier: ")
-os.system('git commit -m '+info+' ')
+os.system("git commit -m ""Ajoutrécent"" ")
 os.system("git push -u PythonProject master")
