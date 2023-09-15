d9d8e4e056e8c3d0b886cb468d8f9a968ccec5ca
unknown <briaglp2@gmail.com>
2023-09-15 14:33:01 +0200

    ajoutdufichierintertxt

--- autogithub.py
+++ autogithub.py
@@ -3,5 +3,5 @@ import os
 
 info = input("Entrer les informations qui ont été modifier: ")
 os.system("git add .")
-os.system("git commit -m "+info+" ")
+os.system('git commit -m '+info+' ')
 os.system("git push -u PythonProject master")
